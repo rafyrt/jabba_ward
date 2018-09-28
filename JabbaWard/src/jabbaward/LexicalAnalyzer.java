0d21eb39a22e391639a0a0fee296d362c1f62398
Rafael Reyes <rafaelt@cetys.edu.mx>
2018-09-27 19:30:38 -0700

    Cambios

--- JabbaWard/src/jabbaward/LexicalAnalyzer.java
+++ JabbaWard/src/jabbaward/LexicalAnalyzer.java
@@ -9,6 +9,8 @@ package jabbaward;
  *
  * @author Rafael, Felipe & Cesar
  */
+
+import java.util.regex.*;
 public class LexicalAnalyzer {
     String archivoFuente;
     String tablaSimbolos;
@@ -16,7 +18,39 @@ public class LexicalAnalyzer {
         archivoFuente = archivo;
     }
     
-    public void read(String source){
+    public void read(){
         
     }
+}
+
+class MDD{
+    int posicionInicial, posicionActual;
+    public MDD(){
+        
+    }
+    /*
+    9 comentario
+    12 comparacion aritmetica
+    10 comparacion logica
+    11 palabra reservada
+    0 asignacion
+    1 EOL
+    2 ewok
+    3 wookie
+    4 aritmetico
+    5 agrupador
+    7 drones
+    8 clones
+    6 id
+        
+    */
+    public int categoriza(String code, int pIni){
+        boolean encontro = false;
+        while(!encontro){
+            
+        }
+        
+        return 0;
+    }
+    
 }
