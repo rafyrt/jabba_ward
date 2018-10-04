a1db50404c1686dbc6d4452e16cff36641b7a3db
CesarTorres2596 <37921177+CesarTorres2596@users.noreply.github.com>
2018-10-03 19:03:28 -0700

    Reader
    .-.

--- JabbaWard/src/jabbaward/JabbaWard.java
+++ JabbaWard/src/jabbaward/JabbaWard.java
@@ -5,6 +5,12 @@
  */
 package jabbaward;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 /**
  *
  * @author Rafael
@@ -16,6 +22,23 @@ public class JabbaWard {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String filePath = ".\\test.jarjar";
+        System.out.println( fileReader( filePath ) );
+    }
+
+     private static String fileReader(String filePath)
+    {
+        StringBuilder contentBuilder = new StringBuilder();
+ 
+        try (Stream<String> stream = Files.lines( Paths.get(filePath), StandardCharsets.UTF_8))
+        {
+            stream.forEach(s -> contentBuilder.append(s).append("\n"));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return contentBuilder.toString();
     }
     
 }
