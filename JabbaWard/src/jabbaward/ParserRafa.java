8723709a26713ff5b434d45fecc62608f0260f76
Rafael Reyes <rafaelt@cetys.edu.mx>
2018-10-25 02:21:27 -0700

    2

--- JabbaWard/src/jabbaward/ParserRafa.java
+++ JabbaWard/src/jabbaward/ParserRafa.java
@@ -13,56 +13,79 @@ import java.util.ArrayList;
  */
 public class ParserRafa {
     ArrayList<Symbol> symbols;
+    int posicion;
     
     public ParserRafa(ArrayList<Symbol> symbols){
         this.symbols = symbols;
     }
-    boolean isLines( int posicion){
-        if (isLine(posicion)) {
-            
-            
+    public boolean isS(){
+        return isLines();
+    }
+    boolean isEmpty(){
+        return symbols.get(posicion).getCategoria().equals("EOL")||symbols.get(posicion).getValor().equals("}");
+    }
+    boolean isLines(){
+        if (isLine()) {
+            return isLines();
+        }else if(isEmpty())
+            return true;
+        return false;
+    }
+    boolean isLine(){
+        return isDecl()|| isIf();
+    }
+    boolean isIf(){
+        if (symbols.get(posicion).getValor().equals("do")) {
+            if (symbols.get(posicion + 1).getValor().equals("(")) {
+                if (isLogic()) {
+                    if (symbols.get(posicion +2).getValor().equals(")")) {
+
+                    }
+                }
+            }
         }
         return false;
     }
-    boolean isLine( int posicion){
-        return isDecl(posicion);
+    boolean isLogic(){
+        return false;
     }
-    public boolean isDecl( int posicion){
-        int offset = 0;
+    public boolean isDecl(){
         if (symbols.get(posicion).getCategoria().equals("tipo de dato")) {
-            if (isID(posicion + 1) && symbols.get(posicion+2).getCategoria().equals("EOL")) {
+            if (isID() && symbols.get(posicion).getCategoria().equals("EOL")) {
                     return true;                                 
             }
-            return isAsign( posicion + 1);
+            return isAsign();
         }
         return false;
     }
     
-    boolean isAsign( int posicion){
-        if (isID( posicion)) {
+    boolean isAsign(){
+        if (isID()) {
             if (symbols.get(posicion+1).getCategoria().equals("asignacion")) {
-                return isCalc(posicion +2);
+                if(isCalc()){
+                    
+                }
             }
         }
         return false;
     }
-    boolean isCalc( int posicion){
-        if (isVar( posicion)){
-            if (symbols.get(posicion + 1).getCategoria().equals("operador aritmetico")) {
-                return isCalc( posicion + 2);
+    boolean isCalc(){
+        if (isVar( )){
+            if (symbols.get(posicion).getCategoria().equals("operador aritmetico")) {
+                return isCalc( );
             }
             return true;
         }
         return false;
     }
-    boolean isVar( int posicion){
-        return isID( posicion) || isConstant( posicion);
+    boolean isVar( ){
+        return isID( ) || isConstant( );
     }
-    boolean isConstant( int posicion){
+    boolean isConstant(){
         return symbols.get(posicion).getCategoria().equals("clones") || symbols.get(posicion).getCategoria().equals("lightsaber")
                 || symbols.get(posicion).getCategoria().equals("ewok") || symbols.get(posicion).getCategoria().equals("wookie");
     }
-    boolean isID( int posicion){
+    boolean isID(){
         return symbols.get(posicion).getCategoria().equals("id");
     }
     
